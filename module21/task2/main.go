f871db06145764a569e8a564789196285f47ada6
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-04 21:51:13 +0300

    m21t2 done

--- module21/task2/main.go
+++ module21/task2/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func B(A func(int, int) int) {
+	defer fmt.Println("B вызывает A с аргументами 5 и 4, получает:", A(5, 4))
+	fmt.Println("Начало работы B")
+}
+
+func main() {
+	B(func(i int, j int) int {
+		i++
+		j--
+		return i + j
+	})
+
+	B(func(i int, j int) int {
+		return i*i + j*j
+	})
+
+	B(func(i int, j int) int {
+		return (i - j) * (i + j)
+	})
 }
