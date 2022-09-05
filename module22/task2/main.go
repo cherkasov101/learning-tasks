0e99ff6159cf20cdb68c4a5c3226ce27ef1dd1e9
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-05 16:09:33 +0300

    m22t2 done

---
+++ module22/task2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+const size = 12
+
+func makeArray() (array [size]int) {
+	fmt.Println("Введите упорядоченный массив:")
+	for i := range array {
+		if _, err := fmt.Scan(&array[i]); err != nil {
+			fmt.Println("Некорректный ввод.")
+		}
+	}
+	return
+}
+
+func searchNumber(array [size]int, num int) (answer int) {
+	answer = -1
+	min := 0
+	max := size - 1
+	for max >= min {
+		middle := (max + min) / 2
+		if num == array[middle] {
+			answer = middle
+			middle--
+			for middle >= 0 {
+				if array[middle] == num {
+					answer = middle
+					middle--
+				} else {
+					break
+				}
+			}
+			break
+		} else if num > array[middle] {
+			min = middle + 1
+		} else {
+			max = middle - 1
+		}
+	}
+	return
+}
+
+func main() {
+	array := makeArray()
+	var num int
+	fmt.Println("Введите число:")
+	if _, err := fmt.Scan(&num); err != nil {
+		fmt.Println("Некорректный ввод.")
+	}
+
+	index := searchNumber(array, num)
+	if index == -1 {
+		fmt.Println("Числа нет в массиве.")
+		return
+	}
+	fmt.Println("Индекс первого вхождения заданного числа в массив:", searchNumber(array, num))
+}
