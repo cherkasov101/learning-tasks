cb06910208003f6aff4af37d0d95eb477f759b7c
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-05 20:39:39 +0300

    m23t1 done

---
+++ module23/task1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	even, odd := evenAndOdd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println("Массив чётных чисел:", even)
+	fmt.Println("Массив нечётных чисел:", odd)
+}
+
+func evenAndOdd(array ...int) (even, odd []int) {
+	for _, i := range array {
+		if i%2 == 0 {
+			even = append(even, i)
+		} else {
+			odd = append(odd, i)
+		}
+	}
+	return
+}
