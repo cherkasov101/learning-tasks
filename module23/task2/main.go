8784cd0c809c35b7a6d4ea35511bcc387909a79c
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-11 13:03:43 +0300

    m23t2 done

--- module23/task2/main.go
+++ module23/task2/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+// parseTest - function for parsing the last word in a sentence into runes
+func parseTest(sentences [4]string, chars [5]rune) ([4][5]int, error) {
+	var answerArray [4][5]int
+	if len(sentences) == 0 || len(chars) == 0 {
+		return answerArray, fmt.Errorf("the array is empty")
+	}
+
+	for i, s := range sentences {
+		s = strings.ToUpper(s)
+		words := strings.Fields(s)
+		lastWord := words[len(words)-1]
+		for j, ch := range chars {
+			index := -1
+			if strings.Contains(lastWord, string(ch)) {
+				index = strings.Index(lastWord, string(ch))
+			}
+			answerArray[i][j] = index
+		}
+	}
+
+	return answerArray, nil
+}
+
+func main() {
+	sentences := [4]string{"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
+	chars := [5]rune{'H', 'E', 'L', 'П', 'М'}
+	answerArray, err := parseTest(sentences, chars)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i, s := range sentences {
+		fmt.Println("Sentence:", s)
+		for j, ch := range chars {
+			if answerArray[i][j] >= 0 {
+				fmt.Printf("'%c' position %d\n", ch, answerArray[i][j])
+			}
+		}
+	}
 }
