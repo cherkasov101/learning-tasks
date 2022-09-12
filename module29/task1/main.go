1e2cd494e03a9bcc2b2bed2550eea1f36d00286b
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-12 20:08:33 +0300

    m29t1 done

---
+++ module29/task1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	var str string
+	numChan := make(chan int)
+	for {
+		var wg sync.WaitGroup
+		wg.Add(2)
+		fmt.Println("Введите число:")
+		fmt.Scan(&str)
+		if str == "стоп" {
+			break
+		}
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			fmt.Println("Некорректный ввод.")
+		}
+		go square(num, &wg, numChan)
+		go multByTwo(&wg, numChan)
+		wg.Wait()
+	}
+	defer close(numChan)
+}
+
+func square(num int, wg *sync.WaitGroup, numChan chan int) {
+	defer wg.Done()
+	num = num * num
+	fmt.Println("Квадрат:", num)
+	numChan <- num
+}
+
+func multByTwo(wg *sync.WaitGroup, numChan chan int) {
+	defer wg.Done()
+	num := (<-numChan) * 2
+	fmt.Println("Произведение :", num)
+}
