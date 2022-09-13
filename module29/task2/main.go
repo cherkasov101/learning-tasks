6e241dea3fcb5b441effbf3a1de036c6c4ebd370
Iurii Cherkasov <cherkasov1010@gmail.com>
2022-09-13 22:58:25 +0300

    m29t2 done

--- module29/task2/main.go
+++ module29/task2/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+)
 
+func main() {
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			select {
+			case <-shutdown:
+				fmt.Println("Выход из программы")
+				return
+			default:
+				var num int
+				fmt.Println("Введите число:")
+				fmt.Scan(&num)
+				fmt.Println("Квадрат:", num*num)
+			}
+		}
+	}()
+
+	wg.Wait()
 }
